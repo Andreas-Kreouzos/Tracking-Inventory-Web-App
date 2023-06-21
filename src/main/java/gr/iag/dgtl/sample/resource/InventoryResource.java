b97113328a370c8c7bb4e5faefdb2e6db8f38ff2
kreouzosa <kreouzosa@interamerican.gr>
2023-06-21 17:08:26 +0300

    Deleted SampleDto and modified Resource

--- src/main/java/gr/iag/dgtl/sample/resource/InventoryResource.java
+++ src/main/java/gr/iag/dgtl/sample/resource/InventoryResource.java
@@ -1,12 +1,11 @@
-package gr.iag.dgtl.sample;
+package gr.iag.dgtl.sample.resource;
 
-import jakarta.inject.Inject;
+import gr.iag.dgtl.sample.dto.Item;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -15,21 +14,17 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/sample")
-public class SampleResource {
-
-    @Inject
-    @ConfigProperty(name = "app.sample.property")
-    private String prop;
+public class InventoryResource {
 
     @Operation(summary = "Sample API that returns the value of the sample property in a DTO")
     @APIResponses(value = {
             @APIResponse(responseCode = "200", description = "Success",
                 content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = SampleDto.class, type = SchemaType.ARRAY))})
+                schema = @Schema(implementation = Item.class, type = SchemaType.ARRAY))})
     })
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProperty(){
-        return Response.ok(new SampleDto(prop)).build();
+    public Response getItems(){
+        return null;
     }
 }
