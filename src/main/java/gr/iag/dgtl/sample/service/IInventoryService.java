43572a2eadc797f5bd5aab2477ef18799b774328
kreouzosa <kreouzosa@interamerican.gr>
2023-06-21 17:18:00 +0300

    Added javadoc for the service interface

--- src/main/java/gr/iag/dgtl/sample/service/IInventoryService.java
+++ src/main/java/gr/iag/dgtl/sample/service/IInventoryService.java
@@ -11,8 +11,30 @@ import java.util.Optional;
  */
 public interface IInventoryService {
 
+    /**
+     * Through this method, a list of items gets returned
+     * @return the List of Items
+     */
     List<Item> getItems();
+
+    /**
+     * Adds an Item into inventory
+     * @param name the name of the Item
+     * @param serialNumber the serial number of the Item
+     * @param value the value in $ of the Item
+     */
     void addItem(String name, String serialNumber, BigDecimal value);
+
+    /**
+     * Gets an Item from the inventory by using the serial number
+     * @param serialNumber the serial number of the Item
+     * @return an Optional of the Item
+     */
     Optional<Item> getItemBySerialNumber(String serialNumber);
+
+    /**
+     * Deletes an Item from the inventory by using the serial number
+     * @param serialNumber the serial number of the Item
+     */
     void deleteItem(String serialNumber);
 }
