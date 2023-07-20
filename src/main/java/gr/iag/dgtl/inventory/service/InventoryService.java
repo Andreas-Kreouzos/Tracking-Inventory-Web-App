63150e7e44558e45c30429bcacdf0a39643caaec
kreouzosa <kreouzosa@interamerican.gr>
2023-07-20 17:33:39 +0300

    Modified item insertion, added item deletion test

--- src/main/java/gr/iag/dgtl/inventory/service/InventoryService.java
+++ src/main/java/gr/iag/dgtl/inventory/service/InventoryService.java
@@ -1,8 +1,8 @@
 package gr.iag.dgtl.inventory.service;
 
 import gr.iag.dgtl.inventory.dto.Item;
+import gr.iag.dgtl.inventory.persistance.IItemPersistence;
 import jakarta.enterprise.context.ApplicationScoped;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +10,14 @@ import java.util.Optional;
 @ApplicationScoped
 public class InventoryService implements IInventoryService {
 
-    private final List<Item> items = new ArrayList<>();
+    private final List<Item> items;
+    private final IItemPersistence itemPersistence;
+
+    public InventoryService(IItemPersistence itemPersistence) {
+        this.itemPersistence = itemPersistence;
+        List<Item> loadedItems = itemPersistence.loadItems();
+        this.items = loadedItems != null ? loadedItems : new ArrayList<>();
+    }
 
     @Override
     public List<Item> getItems() {
@@ -20,15 +27,21 @@ public class InventoryService implements IInventoryService {
     @Override
     public void addItem(Item item) {
         items.add(item);
+        itemPersistence.saveItems(items);
     }
 
     @Override
     public Optional<Item> getItemBySerialNumber(String serialNumber) {
-        return Optional.empty();
+        return items.stream()
+                .filter(item -> item.serialNumber().equals(serialNumber))
+                .findFirst();
     }
 
     @Override
     public void deleteItem(String serialNumber) {
-
+        getItemBySerialNumber(serialNumber).ifPresent(item -> {
+            items.remove(item);
+            itemPersistence.saveItems(items);
+        });
     }
 }
--- src/test/groovy/gr/iag/dgtl/inventory/service/InventoryServiceSpec.groovy
+++ src/test/groovy/gr/iag/dgtl/inventory/service/InventoryServiceSpec.groovy
@@ -1,6 +1,7 @@
 package gr.iag.dgtl.inventory.service
 
 import gr.iag.dgtl.inventory.dto.Item
+import gr.iag.dgtl.inventory.persistance.IItemPersistence
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -8,23 +9,51 @@ class InventoryServiceSpec extends Specification {
 
     @Subject
     InventoryService service
+    IItemPersistence itemPersistence
 
     def setup() {
-        service = new InventoryService()
+        itemPersistence = Mock(IItemPersistence)
+        service = new InventoryService(itemPersistence)
     }
 
-    def 'Successfully insert an Item into list'() {
+    def cleanup() {
+        File file = new File('inventory.json')
+        if (file.exists()) {
+            file.delete()
+        }
+    }
+
+    def 'Successfully insert an item into list'() {
         given: 'an item'
         def item = new Item('Xbox One','AXB124AXY', 500 as BigDecimal)
 
-        when: 'calling the service'
+        and: 'persistence returns empty list initially and then adds the item'
+        itemPersistence.loadItems() >> []
+        itemPersistence.saveItems(_) >> { args -> args[0] }
+
+        when: 'trying to add the item in the list'
         service.addItem(item)
 
-        then: 'the item is added to the service'
+        then: 'the item is indeed being added'
         def items = service.getItems()
         items.size() == 1
         items.get(0).name() == item.name()
         items.get(0).serialNumber() == item.serialNumber()
         items.get(0).value() == item.value()
+    }
+
+    def 'Successfully delete an item from the list'() {
+        given: 'an item'
+        def item = new Item('Xbox One','AXB124AXY', 500 as BigDecimal)
+
+        and: 'persistence initially returns list with item and then removes the item'
+        itemPersistence.loadItems() >> [item]
+        itemPersistence.saveItems(_) >> { args -> args[0] }
+
+        when: 'trying to delete the item from the list'
+        service.deleteItem(item.serialNumber())
+
+        then: 'the item is indeed being removed'
+        service.getItems().isEmpty()
     }
 }
