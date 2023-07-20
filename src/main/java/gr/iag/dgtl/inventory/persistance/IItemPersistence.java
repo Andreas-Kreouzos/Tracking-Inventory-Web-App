027f0180e6ecbca2647827fb6133ed68e4f8c80d
kreouzosa <kreouzosa@interamerican.gr>
2023-07-20 17:31:16 +0300

    Interface to persist data in JSON, HTML and CSV

---
+++ src/main/java/gr/iag/dgtl/inventory/persistance/IItemPersistence.java
@@ -0,0 +1,26 @@
+package gr.iag.dgtl.inventory.persistance;
+
+import gr.iag.dgtl.inventory.dto.Item;
+
+import java.util.List;
+
+/**
+ * Defines the contract for the persistence of items
+ *
+ * @author kreouzosa
+ */
+public interface IItemPersistence {
+    /**
+     * Loads items from a JSON, HTML or CSV file
+     *
+     * @return a list of items, which can be empty if no items are found
+     */
+    List<Item> loadItems();
+
+    /**
+     * Saves the given list of items to JSON, HTML or CSV file
+     *
+     * @param items the list of items to be saved
+     */
+    void saveItems(List<Item> items);
+}
